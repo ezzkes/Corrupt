be1ba8f3dfd5dd7120278373ab0e023e4d1e6629
ezzkes <95560170+ezzkes@users.noreply.github.com>
2022-03-12 20:23:05 -1000

    Update game_page.js

--- game_page.js
+++ game_page.js
@@ -27,3 +27,32 @@ row = question_word + input_box + check_button ;
 document.getElementById("output").innerHTML = row;
 document.getElementById("word").value = "";  
 }
+question_turn="player1"
+anwser_turn="player2"
+funtion check(){
+get_answer=document.getById("input_check_box").value
+answer=get_answer.toLowerCase()
+console.log("answerinLowerCase"+answer")
+if(answer==word){
+if(answer_turn=="player1"){
+player1_score=player1_score+1
+document.getElementById("player1_score").innerhtml=player1_score:
+}
+else{
+player2_score=player2_score+1
+document.getElementById("player2_score").innerhtml=player2_score:
+}
+}
+if(question_turn == "player1")
+{ question_turn = "player2" document.getElementById("player_question").innerHTML = "Question Turn - " + player2_name ;
+}
+else { question_turn = "player1" document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name ;
+}
+if(answer_turn == "player1")
+{ 
+answer_turn = "player2" document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name ;
+} 
+else { answer_turn = "player1" document.getElementById("player_answer").innerHTML = "Answer Turn - " + player1_name ;
+}
+document.getElementById("output").innerHTML = "";
+}
